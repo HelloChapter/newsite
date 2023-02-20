5c32f119c7fb87723f79c9779c72c6b70882381e
Aniket Dhadave <aniket@betacraft.com>
2023-02-20 14:30:41 +0530

    Miami pages UI issue fizxes

--- js/custom.js
+++ js/custom.js
@@ -133,7 +133,7 @@ $(".range-slider").on("input change", (e)=>{
     // Update the width of the foreground image
     $('.foreground-img').css('width', `${sliderPos}%`)
     // Update the position of the slider button
-    $('.slider-button').css('left', `calc(${sliderPos}% - 18px)`)
+    $('.slider-button').css('left', `calc(${sliderPos}% - 28px)`)
   });
   $(".after-before-btn-wrap .before-btn").on("input click", (e)=>{
     const sliderPos = e.target.value;
