60e923dd4e576de578a998a0a54dc436151274f8
Aniket Dhadave <aniket@betacraft.com>
2023-02-21 17:40:58 +0530

    Miami projects slider issue fixes

--- js/custom.js
+++ js/custom.js
@@ -128,12 +128,19 @@ jQuery(document).ready(function ($) {
 });
 /*Ready function end*/
 // After Before Js start
-$(this).on("input change", (e)=>{
+$(".range-slider1").on("input change", (e)=>{
     const sliderPos = e.target.value;
     // Update the width of the foreground image
-    $('.foreground-img').css('width', `${sliderPos}%`)
+    $('.two-joint-images1 .foreground-img').css('width', `${sliderPos}%`)
     // Update the position of the slider button
-    $('.slider-button').css('left', `calc(${sliderPos}% - 28px)`)
+    $('.two-joint-images1 .slider-button').css('left', `calc(${sliderPos}% - 28px)`)
+  });
+  $(".range-slider2").on("input change", (e2)=>{
+    const sliderPos = e2.target.value;
+    // Update the width of the foreground image
+    $('.two-joint-images2 .foreground-img').css('width', `${sliderPos}%`)
+    // Update the position of the slider button
+    $('.two-joint-images2 .slider-button').css('left', `calc(${sliderPos}% - 28px)`)
   });
   $(".after-before-btn-wrap .before-btn").on("input click", (e)=>{
     const sliderPos = e.target.value;
@@ -142,11 +149,25 @@ $(this).on("input change", (e)=>{
     // Update the position of the slider button
     $('.slider-button').css('left', `calc(100% - 40px)`)
   });
-  $(".after-before-btn-wrap .after-btn").on("input click", (e)=>{
+  $(".two-joint-images1 .after-before-btn-wrap .after-btn").on("input click", (e)=>{
     const sliderPos = e.target.value;
     // Update the width of the foreground image
-    $('.foreground-img').css('width', '0%')
+    $('.two-joint-images1 .foreground-img').css('width', '0%')
     // Update the position of the slider button
-    $('.slider-button').css('left', `calc(0px)`)
+    $('.two-joint-images1 .slider-button').css('left', `calc(0px)`)
+  });
+  $(".two-joint-images2 .after-before-btn-wrap .before-btn").on("input click", (e)=>{
+    const sliderPos = e.target.value;
+    // Update the width of the foreground image
+    $('.two-joint-images2 .foreground-img').css('width', '100%')
+    // Update the position of the slider button
+    $('.two-joint-images2 .slider-button').css('left', `calc(100% - 40px)`)
+  });
+  $(".two-joint-images2 .after-before-btn-wrap .after-btn").on("input click", (e)=>{
+    const sliderPos = e.target.value;
+    // Update the width of the foreground image
+    $('.two-joint-images2 .foreground-img').css('width', '0%')
+    // Update the position of the slider button
+    $('.two-joint-images2 .slider-button').css('left', `calc(0px)`)
   });
   // After Before Js end
