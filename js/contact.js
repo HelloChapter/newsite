d59c30b625e5887b05b6d7ac82406983cf30c332
Aniket Dhadave <aniket@betacraft.com>
2023-03-08 11:58:23 +0530

    Contact validation issue fixes

--- js/contact.js
+++ js/contact.js
@@ -1,106 +1,126 @@
 var isValid = true;
 // default values
 var postDataObject = {
-	"firstName": "",
-	"lastName": "",
+  "firstName": "",
+  "lastName": "",
   "email": "",
-	"phoneNumber": "",
-	"city": "",
-	"zipCode": "",
-  "projectDescription":"",
+  "phoneNumber": "",
+  "city": "",
+  "zipCode": "",
+  "projectDescription": "",
 }
 // Email Validation
 function isEmail(email) {
-    return /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/.test(email)
+  return /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/.test(email)
 }
 // On input change
-$(document).on("change", "input, textarea", (function(e) {
+$(document).on("change", "input, textarea", (function (e) {
 
-    if(e.target.value === ""){
-      isValid=false;
-      $(e.target).parent().addClass("error");
-      return
-    }else if(($(e.target).attr("name") === "email" && !isEmail(e.target.value))){
-      isValid=false;
-      $(e.target).parent().addClass("error");
-      return
-    }else if(($(e.target).attr("name") === "streetAddress" && e.target.value.length < 5)){
-      isValid=false;
-      $(e.target).parent().addClass("error");
-      return
-    }else if(((!/^[0-9]+$/.test(e.target.value) && $(e.target).attr("name") === "phoneNumber") || ($(e.target).attr("name") === "phoneNumber" && (e.target.value.length < 5 || e.target.value.length > 12 )))){
-      isValid=false;
-      $(e.target).parent().addClass("error");
-      return
-    }else {
-      postDataObject[e.target.getAttribute("name")] = e.target.value;
-      $(e.target).parent().removeClass("error");
-      return
-    }
-}));
-$(document).on("change", "select", (function(e) {
+  if (e.target.value === "") {
+    isValid = false;
+    $(e.target).parent().addClass("error");
+    return
+  } else if (($(e.target).attr("name") === "email" && !isEmail(e.target.value))) {
+    isValid = false;
+    $(e.target).parent().addClass("error");
+    return
+  } else if (($(e.target).attr("name") === "streetAddress" && e.target.value.length < 5)) {
+    isValid = false;
+    $(e.target).parent().addClass("error");
+    return
+  } else if (((!/^[0-9]+$/.test(e.target.value) && $(e.target).attr("name") === "phoneNumber") || ($(e.target).attr("name") === "phoneNumber" && (e.target.value.length < 5 || e.target.value.length > 12)))) {
+    isValid = false;
+    $(e.target).parent().addClass("error");
+    return
+  } else {
     postDataObject[e.target.getAttribute("name")] = e.target.value;
     $(e.target).parent().removeClass("error");
     return
+  }
 }));
+$(document).on("change", "select", (function (e) {
+  postDataObject[e.target.getAttribute("name")] = e.target.value;
+  $(e.target).parent().removeClass("error");
+  return
+}));
+jQuery(document).ready(function ($) {
+  var inputQuantity = [];
+  $("#phoneNumber").on("keyup", function (e) {
+    var $field = $(this);
+    val = this.value;
+    console.log(val);
+    $thisIndex = parseInt($field.data("idx"), 10);
+    if (this.validity && this.validity.badInput || isNaN(val) || $field.is(":invalid")) {
+      this.value = inputQuantity[$thisIndex];
+      return;
+    }
+    if (val.length > Number($field.attr("maxlength"))) {
+      val = val.slice(0, 12);
+      $field.val(val);
+    }
+    inputQuantity[$thisIndex] = val;
+  });
+});
+/*Ready function end*/
 window.fbq('track', 'Contact', { value: 0, currency: 'USD' });
 // Ajax function
 function makeAjaxCall(url, type, crossDomain, dataObject, callback) {
-    $.ajax({
-        dataType: "json",
-        url: url,
-        type: type,
-        data: dataObject,
-        crossDomain: crossDomain,
-        async: !1,
-        success: function(result) {
-            callback(result)
-        },
-        error: function(jqXHR, exception) {
-            inputs.each(function(){
-              $(this).attr("disabled", "false");
-            })
-        }
-    })
+  $.ajax({
+    dataType: "json",
+    url: url,
+    type: type,
+    data: dataObject,
+    crossDomain: crossDomain,
+    async: !1,
+    success: function (result) {
+      callback(result)
+    },
+    error: function (jqXHR, exception) {
+      inputs.each(function () {
+        $(this).attr("disabled", "false");
+      })
+    }
+  })
 }
 // Redirect user on submit
-function redirectToThankYou(){
-    window.location.pathname = "/thank-you-message/"
+function redirectToThankYou() {
+  window.location.pathname = "/thank-you-message/"
 }
 // Contact Submit 
 function handleContactSubmit(e) {
-    var inputs = $("#contact-form input");
-    isValid = true;
-    inputs.each((function() {
-        if(this.value === ""){
-          isValid=false;
-          $(this).parent().addClass("error");
-        }
-        if(($(this).attr("name") === "email" && !isEmail(this.value))){
-          isValid=false;
-          $(this).parent().addClass("error");
-        }
-        if(($(this).attr("name") === "streetAddress" && this.value.length < 5)){
-          isValid=false;
-          $(this).parent().addClass("error");
-        }
-        if(((!/^[0-9]+$/.test(this.value) && $(this).attr("name") === "phoneNumber") || ($(this).attr("name") === "phoneNumber" && (this.value.length < 5 || this.value.length > 12 )))){
-          isValid=false;
-          $(this).parent().addClass("error");
-        }
-    }));
-    if($("textarea[name='projectDescription'").val() === ""){
-      $("textarea[name='projectDescription'").parent().addClass("error");
-      return false;
+  var inputs = $("#contact-form input");
+  isValid = true;
+  inputs.each((function () {
+    if (this.value === "") {
+      isValid = false;
+      $(this).parent().addClass("error");
     }
-    if(isValid){
-      inputs.each(function(){
-        $(this).attr("disabled", "disabled");
-      })
-     // console.log(postDataObject)
-      // window.gtag('event', 'conversion', {'send_to': 'AW-10883092413/R16jCK7357kDEL2fu8Uo'});
-      makeAjaxCall("https://api.hellochapter.dev/api/contact/add", "POST", !0, postDataObject, redirectToThankYou);
+    if (($(this).attr("name") === "email" && !isEmail(this.value))) {
+      isValid = false;
+      $(this).parent().addClass("error");
     }
-    
-    // return false;
+    if (($(this).attr("name") === "streetAddress" && this.value.length < 5)) {
+      isValid = false;
+      $(this).parent().addClass("error");
+    }
+    if (((!/^[0-9]+$/.test(this.value) && $(this).attr("name") === "phoneNumber") || ($(this).attr("name") === "phoneNumber" && (this.value.length < 5 || this.value.length > 12)))) {
+      isValid = false;
+      $(this).parent().addClass("error");
+    }
+
+  }));
+  if ($("textarea[name='projectDescription'").val() === "") {
+    $("textarea[name='projectDescription'").parent().addClass("error");
+    return false;
+  }
+  if (isValid) {
+    inputs.each(function () {
+      $(this).attr("disabled", "disabled");
+    })
+    // console.log(postDataObject)
+    // window.gtag('event', 'conversion', {'send_to': 'AW-10883092413/R16jCK7357kDEL2fu8Uo'});
+    makeAjaxCall("https://api.hellochapter.dev/api/contact/add", "POST", !0, postDataObject, redirectToThankYou);
+  }
+
+  // return false;
 }
