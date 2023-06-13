7b38910a5b3f358b32e9005ef28268991964ec35
Aniket Dhadave <aniket@betacraft.com>
2023-06-13 11:16:33 +0530

    cookie code added

--- js/custom.js
+++ js/custom.js
@@ -16,7 +16,9 @@ jQuery(document).ready(function ($) {
         // else {
         //     console.log("other");
         // }
-        Cookies.set('HelloChapterContactPath',window.location.href, { expires: 30, path: '' })
+        if(Cookies.get('HelloChapterContactPath') === undefined) {
+            Cookies.set('HelloChapterContactPath',window.location.href, { expires: 30, path: '' })
+        }
 
         if(window.location.pathname === '/contact-chapter-home-renovation/'){
            // Cookies.set('HelloChapterContactPath',window.location.href, { expires: 30, path: '' })
