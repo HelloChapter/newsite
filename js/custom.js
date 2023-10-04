67ca88790eee1b0d18e275efb5118e8bb560579d
Aniket Dhadave <aniket@betacraft.com>
2023-10-04 18:31:33 +0530

    Home page plan, build slider added swipe slider

--- js/custom.js
+++ js/custom.js
@@ -187,10 +187,9 @@ jQuery(document).ready(function ($) {
         }
         lastScrollTop = st;
     });
-    var divHeight = $('.home-how-it-works-slider-swiper .slider-item').height();
-    var divTotalHeight = divHeight * 3;
-    var setHeight = $('.empty-height-div');
-    setHeight.height(divTotalHeight);
+
+    calculate_slider_height();
+    
     // home-how-it-works-slider end
 
     $('.after-before-slider').slick({
@@ -332,4 +331,12 @@ $(".two-joint-images .after-before-btn-wrap .after-btn").click(function (e) {
 });
 
 // After Before Js end
-
+window.addEventListener('resize', function(event) {
+    calculate_slider_height();
+}, true);
+function calculate_slider_height () {
+    var divHeight = $('.home-how-it-works-slider-swiper .slider-item').height();
+    var divTotalHeight = divHeight * 3;
+    var setHeight = $('.empty-height-div');
+    setHeight.height(divTotalHeight);
+}
