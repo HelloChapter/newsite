8c4804830e0326046386d932cfc82271cf2bc30b
Aniket Dhadave <aniket@betacraft.com>
2024-03-01 12:19:49 +0530

    Contact page google api integration WIP

--- js/google-api.js
+++ js/google-api.js
@@ -1,66 +1,51 @@
 document.addEventListener('DOMContentLoaded', function () {
-    var input = document.getElementById('autocomplete');
-    var autocomplete = new google.maps.places.Autocomplete(input, {
-        types: ['geocode'], 
-        componentRestrictions: { country: 'us' }, 
-        fields: ['address_components', 'geometry'],
-        strictBounds: true
-    });
+    $(document).ready(function () {
+        $('#autocomplete').on('input', function () {
+            var query = $(this).val();
+            if (query.length > 0) {
+                $.ajax({
+                    url: 'https://api.hellochapter.dev/api/contact/autocomplete',
+                    type: 'POST',
+                    data: { input: query },
+                    success: function (response) {
+                        var suggestions = response.data.suggestions;
+                        console.log(suggestions)
+                        var suggestionList = '';
+                        for (var i = 0; i < suggestions.length; i++) {
+                            var placePrediction = suggestions[i].placePrediction;
+                          var placeId = placePrediction.placeId;
+                            var text = placePrediction.text.text;
 
-    autocomplete.addListener('place_changed', function () {
-        var place = autocomplete.getPlace();
-        if (!place.geometry) {
-            console.log("Place details not found for input: '" + place.name + "'");
-            return;
-        }
+                            var city, state, postalCode;
+                            var structuredFormat = placePrediction.structuredFormat;
+                            if (structuredFormat && structuredFormat.mainText && structuredFormat.secondaryText) {
+                                city = structuredFormat.mainText.text;
+                                state = structuredFormat.secondaryText.text;
+                            }
 
-        var addressComponents = place.address_components;
-        var city, state, postalCode;
-        var hasCity = false, hasState = false, hasPostalCode = false;
-
-        for (var i = 0; i < addressComponents.length; i++) {
-            var component = addressComponents[i];
-            var componentType = component.types[0];
-            
-            if (componentType === 'locality') {
-                city = component.long_name;
-                hasCity = true;
-            } else if (componentType === 'administrative_area_level_1') {
-                state = component.long_name;
-                hasState = true;
-            } else if (componentType === 'postal_code') {
-                postalCode = component.long_name;
-                hasPostalCode = true;
+                            var types = placePrediction.types;
+                            if (types && types.includes('postal_code')) {
+                                postalCode = text.split(', ').pop();
+                            }
+                           // console.log("city",city + "state", state + "postalCode", postalCode );
+                            suggestionList += '<li>' + suggestions[i]?.placePrediction?.text?.text + '</li>';
+                        }
+                        $('#autocomplete-results').html(suggestionList);
+                        $('#autocomplete-results li').click(function () {
+                            var selectedText = $(this).text();
+                            var selectedPlaceId = $(this).data('placeid');
+                            $('#autocomplete').val(selectedText);
+                            $('#selected-place-id').val(selectedPlaceId);
+                            $('#autocomplete-results').html('');
+                        });
+                    },
+                    error: function (xhr, status, error) {
+                        console.error('Error:', status, error);
+                    }
+                });
+            } else {
+                $('#autocomplete-results').html('');
             }
-            debugger;
-        }
-        console.log("city : ",city + " state : ", state + " postalCode : ", postalCode  );
-
-        if (hasCity && hasState) {
-            postDataObject.city = city;
-            postDataObject.state = state;
-        } else {
-            postDataObject.city = null;
-            postDataObject.state = null;
-        }
-        if (hasPostalCode) {
-            postDataObject.zipCode = postalCode;
-        } else {
-            postDataObject.zipCode = null;
-        }
-    });
-    input.addEventListener('input', function() {
-        var inputText = input.value;
-        if (inputText) {
-            var autocompleteService = new google.maps.places.AutocompleteService();
-            autocompleteService.getPlacePredictions({
-                input: inputText,
-                componentRestrictions: { country: 'us' }
-            }, function(predictions, status) {
-                if (status === google.maps.places.PlacesServiceStatus.OK && predictions) {
-                    console.log(predictions);
-                }
-            });
-        }
+        });
     });
 });
